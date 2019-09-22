c7eb26445a948ccb45930cdfddb80f2d349dba68
John Cato <johncato@Johns-MBP.home>
2019-09-21 23:10:00 -0400

    Add Httparty & Nokogiri gems, setup routing and scrapper method.
    Committer: John Cato <jchristopher.cato@gmail.com>

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'httparty', '~> 0.13.7'
+gem 'nokogiri', '~> 1.6', '>= 1.6.8'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def word_scrapper
+        url = "https://www.spanishdict.com/wordoftheday"
+        unparsed_page = HTTParty.get(url)
+        parsed_page = Nokogiri::HTML(unparsed_page)
+        byebug
+        render template: 'word_scrapper' 
+    end
 end
